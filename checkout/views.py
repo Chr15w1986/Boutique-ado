b5863546b03355f644a220c2211770af5cfccbed
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-07-04 17:55:35 +0000

    add secure checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,3 @@
-""" Checkout views and stripe payments """
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
@@ -36,8 +35,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'stripe_public_key',
-        'client_secret': 'intent.client_secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
