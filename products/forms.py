06db89c33a84a21f3dc906a9c6e8ef28d035d9d3
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-07-10 18:11:17 +0000

    add product view and templates to add product

--- products/forms.py
+++ products/forms.py
@@ -5,12 +5,12 @@ from .models import Product, Category
 class ProductForm(forms.ModelForm):
 
     class Meta:
-        model = product
+        model = Product
         fields = '__all__'
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        categories = Category.object.all()
+        categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
