7f327f36078739aebd97e3cdbd349502ff6fbe80
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-07-10 18:37:38 +0000

    finish product admin functionality

--- products/views.py
+++ products/views.py
@@ -24,8 +24,7 @@ def all_products(request):
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
-                sortkey = 'category__name__'
-
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -36,7 +35,7 @@ def all_products(request):
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-        print(Product)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -69,12 +68,23 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
+
 def add_product(request):
-    """ Enables users to add a product to the store """
-    form = ProductForm
+    """ Add a product to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
-        'form': form
+        'form': form,
     }
 
     return render(request, template, context)
