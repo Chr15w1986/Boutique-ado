3002f02bc844c9f52710a7fbe395a1c491f1b1d6
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-07-16 10:46:59 +0000

    link amazon s3 to django

--- custom_storages.py
+++ custom_storages.py
@@ -3,9 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = storage.STATICFILES_LOCATION
-
+    location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
-    location = storage.MEDIAFILES_LOCATION
+    location = settings.MEDIAFILES_LOCATION
